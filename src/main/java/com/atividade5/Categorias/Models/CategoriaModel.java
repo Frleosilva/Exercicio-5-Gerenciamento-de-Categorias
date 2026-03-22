3f68eff803afe2a9d227a8784c657f9f53513b6f
Frleosilva <leonardofratonibs@gmail.com>
2026-03-22 18:01:38 -0300

    first commit

---
+++ src/main/java/com/atividade5/Categorias/Models/CategoriaModel.java
@@ -0,0 +1,45 @@
+package com.atividade5.Categorias.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categorias")
+public class CategoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Nome_Categoria")
+    private String nome;
+
+    @Column(name = "Descricao_categoria")
+    private String descricao;
+
+    public CategoriaModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/test/java/com/atividade5/Categorias/CategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade5.Categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
